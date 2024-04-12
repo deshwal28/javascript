1b3a4d115b2d1e1cbaf45b5d52bb41a41657645e
deshwal28 <118989884+deshwal28@users.noreply.github.com>
2024-04-12 17:18:59 +0000

    commit

---
+++ basis/2_dataTypes.js
@@ -0,0 +1,18 @@
+"user strict";//treat all js code as newer version
+//alert(3+3)//we are using nodejs,not browwser
+//code readability should be high
+let name = "deshwal"
+let age = 20
+let isLoggedIn=false
+//number =>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>value not assign
+//symbol=>uniqueness
+
+//object
+
+console.log(typeof null);//output:object
+console.log(typeof undefined);//output:undefined
