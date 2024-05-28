55104405462a7d43e59fb700b0a2a1cce34db616
deshwal28 <118989884+deshwal28@users.noreply.github.com>
2024-05-28 17:58:59 +0000

    date

---
+++ basis/9_Date.js
@@ -0,0 +1,8 @@
+let myDate=new Date()
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+let myCreatedDate = new Date(2023,0,24)
+console.log(myCreatedDate.toDateString());
+let myTimeStamp=Date.now()
+console.log(myTimeStamp);
