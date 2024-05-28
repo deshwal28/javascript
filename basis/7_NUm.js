3ffa2b5909dd2b39b42815f244973441502e84fe
deshwal28 <118989884+deshwal28@users.noreply.github.com>
2024-05-28 09:24:48 +0000

    comit

---
+++ basis/7_NUm.js
@@ -0,0 +1,10 @@
+const score =400
+console.log(score);
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+const otherNumber = 123.42
+console.log(otherNumber.toPrecision(3));
+const hundreds = 100000
+console.log(hundreds.toLocaleString('en-IN'));
---
+++ basis/8_Maths.js
@@ -0,0 +1,13 @@
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,3,2,1));
+console.log(Math.max(5,6,7,8));
+console.log(Math.random());//Gives value between 0-1
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+const min =10
+const max=20
+console.log(Math.random()*(max-min +1)+min)
